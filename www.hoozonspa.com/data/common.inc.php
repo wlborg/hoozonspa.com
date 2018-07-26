9204c2a9a72460a662bca16661772a75ffaf12d2
chaihongjun <441315628@qq.com>
2018-07-26 14:34:03 +0800

    init

---
+++ www.hoozonspa.com/data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = '121.199.0.239';
+$cfg_dbname = 'hoozonspa_com';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'chj19840913';
+$cfg_dbprefix = 'spa_';
+$cfg_db_language = 'utf8';
+
+
+?>

ebea1f24d88897fd4c19b6c0f1bed7a5e026636e
August1Rush <271945920@qq.com>
2018-08-08 10:58:48 +0800

    更新案例部分
    更新案例部分

--- www.hoozonspa.com/static/js/index_alone.js
+++ www.hoozonspa.com/static/js/index_alone.js
@@ -1,43 +1,29 @@
 
-var video=document.getElementById('video');
-    video.onclick=function(){
-        alert("pl")
-        if(this.paused){
-        this.play();
-        }else{
-             alert("pa")
-        this.pause();
-        }
-    }
-
-
       $(function(){
         //案例显示 24条数据
-       var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      slidesPerColumn: 2,
-       slidesPerGroup : 3,
-      spaceBetween: 30,
-       autoplay:true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      });
+       var swiperMy = new Swiper('.swiper-containerMy', {
+           slidesPerView: 3,
+           slidesPerColumn: 2,
+           slidesPerGroup : 3,
+           spaceBetween: 30,
+           autoplay:true,
+           pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+       });
         $(".swiper-slide").hover(function() {
              $(this).find(".front").fadeIn();
         }, function() {
-           $(this).find(".front").fadeOut();
+            $(this).find(".front").fadeOut();
         });
         // 首页案例部分点击
         $('.swiper-wrapper .swiper-slide').click(function() {
-
-           swiper.autoplay.stop();
+           autoStop(swiperMy);
            var index_Sw=parseInt($(this).index());
-           console.log(index_Sw)
            if(index_Sw>=0&&index_Sw<=5){
                   $('.fullscreen').removeClass('show');
-                 $('.fullscreen').eq(0).addClass('show');
+                  $('.fullscreen').eq(0).addClass('show');
                  slideTo(index_Sw);
            }else  if(index_Sw>=6&&index_Sw<=11){
                  $('.fullscreen').removeClass('show');
@@ -48,21 +34,28 @@ var video=document.getElementById('video');
                  $('.fullscreen').removeClass('show');
                  $('.fullscreen').eq(2).addClass('show');
                   slideTo(index_Sw);
-           }else  if(index_Sw>=18&&index_Sw<=23){
+           }else if(index_Sw>=18&&index_Sw<=23){
                  $('.fullscreen').removeClass('show');
                  $('.fullscreen').eq(3).addClass('show');
-                slideTo(index_Sw);
+                 slideTo(index_Sw);
            }
            function slideTo(index_Sw){
             swiper_caseSix.slideTo(index_Sw);
            }
         $('.header').removeClass('fixed');
         $('.fullscreen .close').click(function() {
-             swiper.autoplay.start();
+            autoStart(swiperMy);
             $('.fullscreen').removeClass('show');
             $('.header ').addClass('fixed');
         })
-    });
-
-      })
+        // 开始循环
+        function autoStart(obj){
+           obj.autoplay.start();
+        }
+         // 开始暂停循环
+        function autoStop(obj){
+           obj.autoplay.stop();
+        }
+     });
+    })
 

556fb0031cab700d1acb73ca4e7406dfd46c41b9
August1Rush <271945920@qq.com>
2018-09-13 14:56:46 +0800

    产品sql对接
    产品sql对接

--- m.hoozonspa.com/include/extend.func.php
+++ m.hoozonspa.com/include/extend.func.php
@@ -3,19 +3,19 @@ function litimgurls($imgid=0)
 {
     global $lit_imglist,$dsql;
     //获取附加表
-    $row = $dsql->GetOne("SELECT c.addtable FROM #@__archives AS a LEFT JOIN #@__channeltype AS c 
+    $row = $dsql->GetOne("SELECT c.addtable FROM #@__archives AS a LEFT JOIN #@__channeltype AS c
                                                             ON a.channel=c.id where a.id='$imgid'");
     $addtable = trim($row['addtable']);
-    
+
     //获取图片附加表imgurls字段内容进行处理
     $row = $dsql->GetOne("Select imgurls From `$addtable` where aid='$imgid'");
-    
+
     //调用inc_channel_unit.php中ChannelUnit类
     $ChannelUnit = new ChannelUnit(2,$imgid);
-    
+
     //调用ChannelUnit类中GetlitImgLinks方法处理缩略图
     $lit_imglist = $ChannelUnit->GetlitImgLinks($row['imgurls']);
-    
+
     //返回结果
     return $lit_imglist;
 }
@@ -26,7 +26,7 @@ function replaceurl($newurl)
 
        $newurl='https://pc.hoozonspa.com'.'/images/defaultpic.gif';
     }else{
-        
+
        $newurl='https://pc.hoozonspa.com'.$newurl;
     }
 return $newurl;
@@ -36,4 +36,65 @@ function replacebodyurl($body)
 {
     $body=preg_replace('/\/uploads\//g', 'https://pc.hoozonspa.com/uploads/', $body);
     return  $body;
+}
+//获取对应产品列表
+function get_list_chanpings($id)
+{
+global $dsql;
+$relateproject="";
+$relatetypeid = 0;
+$index =0;//下标
+$dsql->SetQuery( "SELECT  * FROM #@__archives AS a
+where  a.typeid='$relatetypeid'  and a.arcrank=0 order by id desc limit 10 ");
+$dsql->Execute();
+$ns = $dsql->GetTotalRow();
+while($row=$dsql->GetArray())
+{
+$id = $row["id"];
+$title = cn_substr($row["title"],80,0);
+$urlarray = GetOneArchive($id);
+$url = $urlarray['arcurl'];
+$litpic =replaceurl($row["litpic"]);
+$des1 = replaceurl($row["des1"]);//规格：
+$des2 = replaceurl($row["des2"]);//产地：
+$des3 = replaceurl($row["des3"]);//功效：
+$des4 = replaceurl($row["des4"]);//适用肌肤：
+$des5 = replaceurl($row["des5"]);//主要成分：
+if($ns%2==0){
+         $relateproject.='<div class="box box1">'+
+                    '<div class="g-left">'+
+                       ' <a href="'.$url.'" title="[field:title/]"><img src="'.$litpic.'" alt="'.$title.'"></a>'+
+                   '</div>'+
+                    '<div class="g-right">'+
+                       ' <h3>'.$ns.'</h3>'+
+                       '<h4>[field:title/]</h4>'+
+                        '<p><span> 规格：</span>'.$des1.'</p>'+
+                       ' <p><span>产地：</span>'.$des2.'</p>'+
+                        '<p><span>功效：</span>'.$des3.'</p>'+
+                        '<p><span>适用肌肤：</span>'.$des4.'</p>'+
+                       '<p><span>主要成分：</span>'.$des5.'</p>'+
+                    '</div>'+
+               ' </div>';
+}else {
+             $relateproject.='<div class="box box1">'+
+                    '<div class="g-right">'+
+                       ' <h3>'.$ns.'</h3>'+
+                       '<h4>[field:title/]</h4>'+
+                        '<p><span> 规格：</span>'.$des1.'</p>'+
+                       ' <p><span>产地：</span>'.$des2.'</p>'+
+                        '<p><span>功效：</span>'.$des3.'</p>'+
+                        '<p><span>适用肌肤：</span>'.$des4.'</p>'+
+                       '<p><span>主要成分：</span>'.$des5.'</p>'+
+                    '</div>'+
+                  ' </div>'+
+                 '<div class="g-left">'+
+                       ' <a href="'.$url.'" title="[field:title/]"><img src="'.$litpic.'" alt="'.$title.'"></a>'+
+                   '</div>';
+}
+
+}
+if($ns>0){
+$relateproject=$relateproject;
+}
+return $relateproject;
 }

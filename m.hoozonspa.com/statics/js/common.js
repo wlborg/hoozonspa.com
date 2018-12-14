c190ca34edbd33b3a57d73541cf3b2c3b6032949
August1Rush <271945920@qq.com>
2018-12-14 10:58:18 +0800

    快商通
    快商通

--- m.hoozonspa.com/statics/js/common.js
+++ m.hoozonspa.com/statics/js/common.js
@@ -463,27 +463,32 @@ function addScript(src) {
     }
 }
 function div19Add(){
-     var timer = null
+     var timer = null;
             //  检查dom是否执行完成
         function check() {
-            var dom = document.getElementById('div19')
+            var dom = document.getElementById('div19');
             if(dom) {
-                  $("#div19").addClass('j-consult');
-                  $("#div19").addClass('div19');
-                  // 点击稍后咨询按钮隐藏，12秒后出现
-                 $(document).on("click",".div19",function(){
+                 $(document).on("click","#div19",function(){
                     var href = "https://hztk5.kuaishang.cn/bs/im.htm?cas=56596___868330&fi=65110&ism=1";
                     window.location.href = href;
                      // TwShow();
                  });
+                   $("#div19").on("click",function(){
+                    var href = "https://hztk5.kuaishang.cn/bs/im.htm?cas=56596___868330&fi=65110&ism=1";
+                    window.location.href = href;
+                     // TwShow();
+                 });
+                 $("#div19").addClass('j-consult');
+                  // 点击稍后咨询按钮隐藏，12秒后出现
+                 bindConsultHref();
+                 return;
                 //  清除定时器
                 if(!timer) {
                     clearTimeout(timer);
-
                 }
             } else {
                 //  自我调用
-                timer = setTimeout(check, 0)
+                timer = setTimeout(check, 0);
             }
         }
         //  首次执行
